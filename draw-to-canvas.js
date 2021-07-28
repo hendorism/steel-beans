13b88f4a91e18f64411567bdae71187e970f7e79
Aaron Dull <hendorism@gmail.com>
2021-07-28 12:58:53 -0700

    &#&#&

--- draw-to-canvas.js
+++ draw-to-canvas.js
@@ -4,7 +4,7 @@
 
 
 
-var canvas1 = document.getElementById("myCanvas");
+var canvas1 = document.getElementById("canvas1");
 var ctx1 = canvas1.getContext("2d");
 ctx1.fillStyle = "#FF0000";
 ctx1.fillRect(0, 0, 150, 75);
